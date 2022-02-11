4d81f884f977154d6659f2d9d8b96fc1baaca050
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-11 17:55:48 -0500

    initial commit plus starting to build tables for each necessary category

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE department(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR 30
+);
+
+CREATE TABLE role(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER, 
+);
+
