24c5f1ed8677d239832470809a88af84c2ad5779
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-13 23:02:55 -0500

    updated view employee option to display extra data, began the create employee option

--- server.js
+++ server.js
@@ -102,14 +102,98 @@ viewRoles = () => {
 };
 
 viewEmployees = () => {
-    const sql = `SELECT employee.id AS id, employee.first_name AS first_name, employee.last_name AS last_name, employee.role_id AS role_id, employee.manager_id AS manager_id FROM employee `;
-    connection/*.promise()*/.query(sql, (err, rows) => {
-      if (err) throw (err);
+  const sql = `SELECT employee.id AS id, employee.first_name AS first_name, employee.last_name AS last_name, role.title AS role_title, 
+                department.department_name AS department, role.salary AS salary, employee.manager_id as manager
+                FROM employee, role, department
+                WHERE department.id = role.department_id
+                AND role.id = employee.role_id`
+                // AND employee.manager_id = employee.first_name`
+  connection /*.promise()*/
+    .query(sql, (err, rows) => {
+      if (err) throw err;
       console.table(rows);
     });
-  
-    promptUser();
-  };
+
+  promptUser();
+};
+
+// ADD
+addEmployee = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Please enter the first name of the employee:',
+      validate: (addFirstName) => {
+        if (addFirstName) {
+          return true;
+        } else {
+          console.log('Please enter the first name of the employee:');
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Please enter the last name of the employee:',
+      validate: (addLastName) => {
+        if (addLastName) {
+          return true;
+        } else {
+          console.log('Please enter the last name of the employee:');
+          return false;
+        }
+      },
+    },
+  ])
+  .then(answer => {
+      const newEmployee = [answer.firstName, answer.lastName];
+      const roleSQL = `Select role.id, role.title FROM role`;
+      connection.query(roleSQL, (error, data) => {
+          if (error) throw error;
+          const roles = data.map(({id, title}) => ({name: title, value: id}));
+          inquirer.prompt([
+              {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role?",
+                choices: roles
+              }
+            ])
+            .then(roleAnswer => {
+                const role = roleAnswer.role;
+                newEmployee.push(role);
+                const managerSQL = `SELECT * FROM employee`;
+                connection.query(managerSQL, (error, data) => {
+                    if(error) throw error;
+                    const managers = data.map(({id, first_name, last_name}) => ({name: first_name + " " + last_name, value: id}));
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'manager',
+                            message: "Who is the employee's manager?",
+                            choices: managers
+                        }
+                    ])
+                    .then(managerChoice => {
+                        const manager = managerChoice.role;
+                        newEmployee.push(manager);
+                        const sql = `INSERT INTO employee(first_name, last_name, role_id, manager_id)
+                                    VALUES(?, ?, ?, ?)`;
+                        connection.query(sql, newEmployee, (error) => {
+                            if (error) throw error;
+                            viewEmployees();
+                        });
+                    });
+                });
+            });
+      });
+  });
+};
+
+addRole = () => {};
+addDepartment = () => {};
 
 // // Default response for any other request (Not Found)
 // app.use((req, res) => {
